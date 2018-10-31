9cdf4003d70d9139c45a89cb0851623c3d420978
bglossner <benglossner@gmail.com>
2018-10-31 10:24:25 -0700

    innerjoin

--- Server/sql_wrapper.js
+++ Server/sql_wrapper.js
@@ -48,7 +48,34 @@ function getUsersFromAlpha(connection, start, limit) {
     return users;
 }
 
-function getFriendsFromId(connection, id, )
+function getFollowersFromId(connection, id, limit) {
+    if(limit == -1) {
+        var myQuery = `SELECT Users.user_id, username FROM Users INNER JOIN Followers ON 
+                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${id}`;
+    }
+    else {
+        var myQuery = `SELECT Users.user_id, username FROM Users INNER JOIN Followers ON 
+                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${id} LIMIT ${limit}`;
+    }
+    
+    let users = new Promise((resolve, reject) => connection.query(myQuery, (err, result, fields) => {
+        if (err) {
+            reject(err);
+        }
+        else {
+            let users = [];
+            for(let element of result) {
+                let userRet = {
+                    "user_id" : element.user_id,
+                    "username" : element.username
+                };
+                users.push(userRet);
+            }
+            resolve(users);
+        }
+    }));
+    return users;
+}
 
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -65,10 +92,19 @@ function getUsersIDBased(connection, start, limit) {
 
 function getUsersAlphaBased(connection, start, limit) {
     getUsersFromAlpha(connection, start, limit).then(function(result) {
+        console.log("\n");
+        console.log(result);
+    });
+}
+
+function getFollowers(connection, id, limit) {
+    getFollowersFromId(connection, id, limit).then(function(result) {
+        console.log("\n");
         console.log(result);
     });
 }
 getUsersIDBased(connection, 514, 6);
 getUsersAlphaBased(connection, 514, 6);
 getUsersAlphaBased(connection, 514, -1);
+getFollowers(connection, 514, -1);
 connection.end();
