a41b3ea6667e82728041a8c2432aa204e824068c
bglossner <benglossner@gmail.com>
2018-10-31 11:01:10 -0700

    allinfo

--- Server/sql_wrapper.js
+++ Server/sql_wrapper.js
@@ -48,14 +48,14 @@ function getUsersFromAlpha(connection, start, limit) {
     return users;
 }
 
-function getFollowersFromId(connection, id, limit) {
+function getFollowersFromId(connection, user_id, limit) {
     if(limit == -1) {
         var myQuery = `SELECT Users.user_id, username FROM Users INNER JOIN Followers ON 
-                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${id}`;
+                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${user_id}`;
     }
     else {
         var myQuery = `SELECT Users.user_id, username FROM Users INNER JOIN Followers ON 
-                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${id} LIMIT ${limit}`;
+                        Followers.follower_id = Users.user_id WHERE Followers.user_id = ${user_id} LIMIT ${limit}`;
     }
     
     let users = new Promise((resolve, reject) => connection.query(myQuery, (err, result, fields) => {
@@ -75,6 +75,34 @@ function getFollowersFromId(connection, id, limit) {
         }
     }));
     return users;
+}
+
+function getAllUserInfo(connection, user_id) {
+    let prom = new Promise((resolve, reject) =>
+        getUsersFromIDs(connection, user_id, 1).then((user_obj) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let userObj = user_obj[0];
+                getFollowersFromId(connection, user_id, -1).then((followers) => {
+                    getBoughtItems(connection, user_id, -1).then((bitems) => {
+                        getLikedItems(connection, user_id, -1).then((litems) => {
+                            resolve({
+                                "user_id" : userObj["user_id"],
+                                "username" : userObj["username"],
+                                "followers" : followers,
+                                "bought_items" : bitems,
+                                "liked_items" : litems
+                            });
+                        });
+                    });
+                });
+            }
+        })
+    );
+
+    return prom;
 }
 
 var connection = mysql.createConnection({
@@ -97,15 +125,25 @@ function getUsersAlphaBased(connection, start, limit) {
     });
 }
 
-function getFollowers(connection, id, limit) {
-    getFollowersFromId(connection, id, limit).then(function(result) {
+function getFollowers(connection, user_id, limit) {
+    getFollowersFromId(connection, user_id, limit).then(function(result) {
         console.log("\n");
         console.log(result);
     });
 }
+
+getUsersAlphaBased(connection, user_id) {
+    getAllUserInfo(connection, user_id).then(function(result) {
+        console.log("\n");
+        console.log(result);
+    });
+}
+
 getUsersIDBased(connection, 514, 6);
 getUsersAlphaBased(connection, 514, 6);
 getUsersAlphaBased(connection, 514, -1);
 getFollowers(connection, 514, -1);
+getFollowers(connection, 514, 2);
+getAllUserInfo(connection, 514);
 
 connection.end();
